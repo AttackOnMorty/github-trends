a2fd17c05b3625acfc2b9d7eb226c83575a40dda
Luke Mao [SSW] <ErenJagerM@outlook.com>
2022-12-20 21:20:31 +0800

    refactor: extract functions in starTrends component

--- src/components/starTrends.js
+++ src/components/starTrends.js
@@ -34,67 +34,11 @@ const StarTrends = ({ repos }) => {
             return;
         }
 
-        const transformedRepos = repos.map((item) => {
-            const { full_name, stargazers_count } = item;
-            const pages = getPagesBy(stargazers_count);
+        const transformedRepos = repos.map(transformRepo);
 
-            return {
-                full_name,
-                pages,
-                stargazers_count,
-            };
-        });
-
-        Promise.all(
-            transformedRepos.map(({ full_name, pages }) => {
-                const [owner, repo] = full_name.split('/');
-                return Promise.all(
-                    pages.map((page) =>
-                        octokit
-                            .request('GET /repos/{owner}/{repo}/stargazers', {
-                                headers: {
-                                    accept: 'application/vnd.github.star+json',
-                                },
-                                owner,
-                                repo,
-                                per_page: 1,
-                                page,
-                            })
-                            .then((res) => res.data[0].starred_at)
-                    )
-                );
-            })
-        ).then((dates) => {
-            let labels;
-            let datasets = [];
-
-            for (let i = 0; i < repos.length; i++) {
-                const { full_name, pages, stargazers_count } =
-                    transformedRepos[i];
-
-                const res = getLabelsAndDataBy(
-                    dates[0],
-                    pages,
-                    stargazers_count
-                );
-
-                // TODO:
-                labels = res.labels;
-
-                datasets.push({
-                    label: full_name,
-                    data: res.data,
-                    spanGaps: true,
-                    borderColor: 'rgb(255, 99, 132)',
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                });
-            }
-
-            setData({
-                labels,
-                datasets,
-            });
-        });
+        Promise.all(transformedRepos.map(getDate)).then((dates) =>
+            setData(getDataBy(dates, transformedRepos))
+        );
     }, [repos]);
 
     const options = {
@@ -113,56 +57,115 @@ const StarTrends = ({ repos }) => {
     return <div>{data && <Line options={options} data={data} />}</div>;
 };
 
-function getPagesBy(stargazersCount) {
-    const res = [];
-    let index = 1;
+function transformRepo(repo) {
+    const { full_name, stargazers_count } = repo;
+    const pages = getPagesBy(stargazers_count);
 
-    const count =
-        stargazersCount > GITHUB_COUNT_LIMIT
-            ? GITHUB_COUNT_LIMIT
-            : stargazersCount;
-    const interval = Math.floor(count / MAX_REQUEST_AMOUNT);
+    return {
+        full_name,
+        pages,
+        stargazers_count,
+    };
 
-    res.push(index);
+    function getPagesBy(stargazersCount) {
+        const res = [];
+        let index = 1;
 
-    for (let i = 0; i < MAX_REQUEST_AMOUNT; i++) {
-        index += interval;
-        res.push(index <= count ? index : count);
-    }
+        const count =
+            stargazersCount > GITHUB_COUNT_LIMIT
+                ? GITHUB_COUNT_LIMIT
+                : stargazersCount;
+        const interval = Math.floor(count / MAX_REQUEST_AMOUNT);
 
-    return res;
-}
+        res.push(index);
 
-function getLabelsAndDataBy(dates, pages, stargazers_count) {
-    const DATE_FORMAT = 'YYYY-MM';
-    const monthDates = dates.map((date) => dayjs(date).format(DATE_FORMAT));
-    const firstDate = monthDates[0];
-    const totalMonths = dayjs().diff(firstDate, 'month');
-
-    const labels = [];
-    const data = [];
-    let currentDate = firstDate;
-
-    for (let i = 0; i < totalMonths; i++) {
-        labels.push(currentDate);
-
-        if (monthDates.includes(currentDate)) {
-            const index = monthDates.indexOf(currentDate);
-            data.push(pages[index]);
-        } else {
-            data.push(null);
+        for (let i = 0; i < MAX_REQUEST_AMOUNT; i++) {
+            index += interval;
+            res.push(index <= count ? index : count);
         }
 
-        currentDate = dayjs(currentDate).add(1, 'month').format(DATE_FORMAT);
+        return res;
     }
+}
 
-    labels.push(dayjs().format(DATE_FORMAT));
-    data.push(stargazers_count);
+function getDate({ full_name, pages }) {
+    const [owner, repo] = full_name.split('/');
+    return Promise.all(
+        pages.map((page) =>
+            octokit
+                .request('GET /repos/{owner}/{repo}/stargazers', {
+                    headers: {
+                        accept: 'application/vnd.github.star+json',
+                    },
+                    owner,
+                    repo,
+                    per_page: 1,
+                    page,
+                })
+                .then((res) => res.data[0].starred_at)
+        )
+    );
+}
+
+function getDataBy(dates, transformedRepos) {
+    let labels;
+    let datasets = [];
+
+    for (let i = 0; i < transformedRepos.length; i++) {
+        const { full_name, pages, stargazers_count } = transformedRepos[i];
+
+        const res = getLabelsAndDataBy(dates[0], pages, stargazers_count);
+
+        // TODO:
+        labels = res.labels;
+
+        datasets.push({
+            label: full_name,
+            data: res.data,
+            spanGaps: true,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        });
+    }
 
     return {
         labels,
-        data,
+        datasets,
     };
+
+    function getLabelsAndDataBy(dates, pages, stargazers_count) {
+        const DATE_FORMAT = 'YYYY-MM';
+        const monthDates = dates.map((date) => dayjs(date).format(DATE_FORMAT));
+        const firstDate = monthDates[0];
+        const totalMonths = dayjs().diff(firstDate, 'month');
+
+        const labels = [];
+        const data = [];
+        let currentDate = firstDate;
+
+        for (let i = 0; i < totalMonths; i++) {
+            labels.push(currentDate);
+
+            if (monthDates.includes(currentDate)) {
+                const index = monthDates.indexOf(currentDate);
+                data.push(pages[index]);
+            } else {
+                data.push(null);
+            }
+
+            currentDate = dayjs(currentDate)
+                .add(1, 'month')
+                .format(DATE_FORMAT);
+        }
+
+        labels.push(dayjs().format(DATE_FORMAT));
+        data.push(stargazers_count);
+
+        return {
+            labels,
+            data,
+        };
+    }
 }
 
 export default StarTrends;
