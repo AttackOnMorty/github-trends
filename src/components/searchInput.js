7f0b1676852c8be890dea248adc40d8b7ce63c32
Luke Mao [SSW] <ErenJagerM@outlook.com>
2022-12-23 10:15:38 +0800

    Render tag

--- src/components/searchInput.js
+++ src/components/searchInput.js
@@ -1,7 +1,17 @@
-import { Select, Spin } from 'antd';
+import { Select, Spin, Tag } from 'antd';
 import { useState } from 'react';
 
 import { getRepositories } from '../api';
+
+const COLORS = [
+    'rgb(54, 162, 235)',
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(153, 102, 255)',
+    'rgb(201, 203, 207)',
+];
 
 let timeout;
 let currentValue;
@@ -61,6 +71,29 @@ const SearchInput = ({ placeholder, style, repos, setRepos }) => {
         setRepos(repos);
     };
 
+    const tagRender = (props) => {
+        const { label, value, closable, onClose } = props;
+        const index =
+            repos.map((repo) => repo.fullName).indexOf(value) % COLORS.length;
+        const onPreventMouseDown = (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+        };
+        return (
+            <Tag
+                color={COLORS[index]}
+                onMouseDown={onPreventMouseDown}
+                closable={closable}
+                onClose={onClose}
+                style={{
+                    marginRight: 3,
+                }}
+            >
+                {label}
+            </Tag>
+        );
+    };
+
     return (
         <Select
             mode="multiple"
@@ -71,6 +104,7 @@ const SearchInput = ({ placeholder, style, repos, setRepos }) => {
             defaultActiveFirstOption={false}
             showArrow={false}
             filterOption={false}
+            tagRender={tagRender}
             onSearch={handleSearch}
             onChange={handleChange}
             notFoundContent={fetching ? <Spin size="small" /> : null}
