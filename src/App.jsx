47453b1a29dc1ff6ab05e6725be9e71c865bb275
Luke Mao [SSW] <ErenJagerM@outlook.com>
2023-02-16 13:30:39 +0800

    feat: update footer

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
                             target="_black"
                             rel="noreferrer"
                         >
-                            Luke
+                            Luke Mao
                         </a>
                     </p>
                 </footer>
