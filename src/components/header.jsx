001249f17943ec89526116ed286e2a251b80bbeb
Luke Mao <ErenJagerM@outlook.com>
2023-08-19 18:54:01 +1000

    feat: add logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,17 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 
+import Logo from '../assets/logo.png';
+
 function Header() {
     return (
         <header className="px-6 sm:px-14 py-4 flex justify-between">
-            <h1 className="text-lg sm:text-2xl font-mono pointer-events-none">
-                GitHub Trends
-            </h1>
+            <div className="flex items-center">
+                <img src={Logo} alt="logo" className="w-10 h-10 mr-4" />
+                <h1 className="text-lg sm:text-2xl font-mono pointer-events-none">
+                    GitHub Trends
+                </h1>
+            </div>
             <a
                 href="https://github.com/AttackOnMorty/github-trends"
                 target="_black"
